72a141e4cce0b7726e650b4dc7f4b585110b8b32
Novozhilov <dndiamond718@gmail.com>
2023-01-24 14:22:48 +0300

    refactoring - input.py - success create list of dict for different templates.

--- input.py
+++ input.py
@@ -1,9 +1,7 @@
-from Badminton_class import Player
-
 import pandas
 import json
 
-day_data_excel = pandas.read_excel('2Расстановки.xlsx', sheet_name='6', header=None)
+day_data_excel = pandas.read_excel('2Расстановки.xlsx', sheet_name='8', header=None)
 
 json_str = day_data_excel.to_json(orient='records', force_ascii=False)
 test = json.loads(json_str)
@@ -13,41 +11,30 @@ temp_players = []
 Pl = list()
 
 side1 = 'Левая площадка'
-side2 = 'Правая площадка'
-def check_none(a):
-    if type(a) != int:
-        return None
-    else:
-        return None
 
-for i, j in zip(test, (0,1,2)) :
+def check_none(a):
+    if type(a) is not None:
+        return a
+
+
+for i, j in zip(test, (0,1)):
     for z, k in zip(i, range(len(i))):
         if i[z] == side1:
             row = int(j+1)
             column = int(k)
-            for m in range(len(test)):
-                check_none(test[m][str(column+1)])
-                temp_players.append(dict(player1=test[int(row)][str(k)],player2=test[int(row)][str(k+1)],player3=test[int(row)][str(k+2)],player4=test[int(row)][str(k+3)], score1=test[int(row+1)][str(k+1)], score2=test[int(row+1)][str(k+2)]))
-                Players.extend(temp_players)
-                temp_players = []
+            if row <= len(test):
+                for m in range(len(test)):
+                    if check_none(test[row+1][str(column+1)]):
+                        temp_players.append(dict(player1=test[int(row)][str(k)],player2=test[int(row)][str(k+1)],player3=test[int(row)][str(k+2)],player4=test[int(row)][str(k+3)], score1=test[int(row+1)][str(k+1)], score2=test[int(row+1)][str(k+2)]))
+                        Players.extend(temp_players)
+                        temp_players = []
+                        row = row + 2
+
                 row = row + 2
 
 
 print(Players)
 
-
-#
-# check = check_none(test[int(3)][str(2)])
-
-#Создал игроков из экселя строки 0
-# for key, value in test[0].items():
-#     if '7' > key > '0':
-#         globals()['player_%s' % key] = Player(value, 500)
-#         Players.append(globals()['player_%s' % key])
-# def players_score(player):
-#     for i in range(len(player)):
-#         z = (player[i].name, player[i].score)
-#         Pl.append(z)
 
 #players_score(Players)
 
