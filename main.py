db0dc8abc5ea45de23ab4896292ec39da9d0bd68
Novozhilov <dndiamond718@gmail.com>
2023-02-02 12:30:32 +0300

    refactoring code. Update main menu. Create conf.ini file with constant arguments.

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ from New_Class import Day
 from input import Import_Excel
 import date_select
 import os
+import configparser
 
 
 
 def data_analyse():
+
+    # Get Excel file name from INI file
+
+    excel_file = read_config('excel_file')
 
     # Input date for new game and validate format of this date
     date = date_select.date_select()
@@ -32,14 +37,14 @@ def data_analyse():
 
     #  If no this date in DB table="games" - import. If exist - pass
     if DB_SQLite.check_date(date):
-        print(f'!   В базе нет игр за ({date}) дату. Эти игры будут добавлены из Excel файла (2Расстановки.xlsx) страница ({date})')
-        if Import_Excel.check_sheet('2Расстановки.xlsx', date):
-            games = Import_Excel.load('2Расстановки.xlsx', date)  #  Convert excel to dict
+        print(f'!   В базе нет игр за ({date}) дату. Эти игры будут добавлены из Excel файла ({excel_file}) страница ({date})')
+        if Import_Excel.check_sheet(excel_file, date):
+            games = Import_Excel.load(excel_file, date)  #  Convert excel to dict
             games = Import_Excel.import_excel(games)              #  Load dict to Players dict format
             for i in games:                                       #  insert all new games to DB table
                 DB_SQLite.insert_games([date, i['player1'], i['player2'], i['player3'], i['player4'], i['score1'], i['score2']])
     else:
-        print(f'!   В базе уже есть игры за ({date}) дату. Игры из из Excel файла (2Расстановки.xlsx) не будут учитываться')
+        print(f'!   В базе уже есть игры за ({date}) дату. Игры из из Excel файла ({excel_file}) не будут учитываться')
 
     #  drop stat table, create stat table
     DB_SQLite.drop_table_stat('stat')
@@ -55,9 +60,21 @@ def data_analyse():
     #  Generate all Statistics Excel report
     DB_SQLite.select_stat1()
 
-    input()
+    #input()
 
 
+def read_config(conf_data):
+    #  Read config file for choose parameters
+    config = configparser.ConfigParser()
+    config.read('conf.ini')
+    if conf_data == 'excel_file':
+        return config['EXCEL']['workbook']
+    elif conf_data == 'ch_start':
+        return config['Championship']['start_day']
+    elif conf_data == 'ch_end':
+        return config['Championship']['end_day']
+    else:
+        return False
 
 if __name__ == '__main__':
 
@@ -65,8 +82,8 @@ if __name__ == '__main__':
     print()
     print('Программа для расчета рейтинга игроков и вычисления победителя по количеству побед в турнирный период')
     print('---------------------------------------------------------------------------------------------------------')
-    print('Выбрав пункт 1 - Программа будет расчитывать рейтинг игроков. Игры заносятся по дням из Excel файл с именем (2Расстановки.xlsx)')
-    print('         Для корректной работы программы Excel файл должен быть шаблоном растановок игр (смотрите шаблон 2Расстановки.xlsx)')
+    print('Выбрав пункт 1 - Программа будет расчитывать рейтинг игроков. Игры заносятся по дням из Excel файл с именем ', read_config('excel_file'))
+    print('         Для корректной работы программы Excel файл должен быть шаблоном растановок игр (смотрите шаблон ', read_config('excel_file'))
     print('         Имя листа в Excel файле должно быть ДАТОЙ игры. Вводимая дата для анализа должна совпадать с именем листа в Excel файле')
     print('         Даты можно вводить в любом порядке, если необходимо обработать несколько дней. Но корректный результат будет после добавления всех дат')
     print('Выбрав пункт 2 - Вы можете удалить из базы игр любой день указав дату. Это может быть неоходимо если при добавлении ирг шаблоном было')
@@ -79,19 +96,29 @@ if __name__ == '__main__':
 
 
     while True:
+        print('    Главное Меню ')
         print('1.  Ввод данных и расчет рейтинга')
         print('2.  Редактирование базы игр (удаление игрового дня)')
         print('0.  Выход из программы')
         print()
         cmd=input('Выберите пункт: ')
+        print()
 
         if cmd == '0':
             break
         elif cmd == '1':
             data_analyse()
         elif cmd == '2':
+            print()
+            print('В разработке......')
+            print()
+            # print(read_config('excel_file'))
+            # print(read_config('ch_start'))
+            # print(read_config('ch_end'))
             pass
-            #return True
+
         else:
+            print()
             print('Попробуйте еще раз')
-    #return True
+            print()
+
