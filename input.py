c345fc3ea65f2a42d8c6c6591fe67d627fa66a1a
Novozhilov <dndiamond718@gmail.com>
2023-02-05 19:39:48 +0300

    refactoring code. Add validation Excel of games for Players Name like - title (all first character = capital).

--- input.py
+++ input.py
@@ -2,10 +2,12 @@ import pandas
 import json
 
 side1 = 'Левая площадка'
+
+
 class Import_Excel:
 
     def load(file, sheet):
-        #Import_Excel.check_sheet(file, sheet)
+        # Import_Excel.check_sheet(file, sheet)
         day_data_excel = pandas.read_excel(file, sheet_name=sheet, header=None)
         json_str = day_data_excel.to_json(orient='records', force_ascii=False)
         test = json.loads(json_str)
@@ -22,7 +24,9 @@ class Import_Excel:
                 return True
         else:
             print()
-            print('!!!!  В Excel файле (%s) нет страницы с датой игрового дня (%s). Переименуйте страницу игрового дня в (%s) и запустите программу еще раз' % (file, sheet, sheet))
+            print(
+                '!!!!  В Excel файле (%s) нет страницы с датой игрового дня (%s). Переименуйте страницу игрового дня в (%s) и запустите программу еще раз' % (
+                file, sheet, sheet))
             print()
             return False
 
@@ -37,8 +41,10 @@ class Import_Excel:
                     column = int(k)
                     if row <= len(test):
                         for m in range(len(test)):
-                            if Import_Excel.check_none(test[row+1][str(column+1)]):
-                                a = dict(player1=test[int(row)][str(k)],player2=test[int(row)][str(k+1)],player3=test[int(row)][str(k+2)],player4=test[int(row)][str(k+3)], score1=test[int(row+1)][str(k+1)], score2=test[int(row+1)][str(k+2)])
+                            if Import_Excel.check_none(test[row + 1][str(column + 1)]):
+                                a = dict(player1=test[int(row)][str(k)].title(), player2=test[int(row)][str(k + 1)].title(),
+                                         player3=test[int(row)][str(k + 2)].title(), player4=test[int(row)][str(k + 3)].title(),
+                                         score1=test[int(row + 1)][str(k + 1)], score2=test[int(row + 1)][str(k + 2)])
                                 temp_players.append(a)
                                 players.extend(temp_players)
                                 temp_players = []
